760dfc276e7f690ae1da7ecfdcb43eaa89932767
Cyuen0310 <chanszeyuen0310@icloud.com>
2026-05-22 18:23:55 +0800

    Add: max-water-container - submission-1

---
+++ Data Structures & Algorithms/max-water-container/submission-1.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     */
+    maxArea(heights) {
+        let max = 0
+        let left = 0, right = heights.length - 1
+        while (left < right){
+            let area = Math.min(heights[left],heights[right]) * (right - left)
+            max = Math.max(area, max)
+            if (heights[left] <= heights[right]) left++
+            else right--
+        }
+        return max
+    }
+}
