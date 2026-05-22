27f4c03828c20ddb90359be6b42b85f3d4c7259a
Cyuen0310 <chanszeyuen0310@icloud.com>
2026-05-23 01:26:05 +0800

    Add: three-integer-sum - submission-8

---
+++ Data Structures & Algorithms/three-integer-sum/submission-8.js
@@ -0,0 +1,37 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     * To do:
+     * make it be a 2sum
+     *
+     */
+    threeSum(nums) {
+        let result = [];
+        let tripletsMap = new Map();
+        nums = nums.sort((a, b) => a - b);
+        console.log(nums);
+        for (let i = 0; i < nums.length; i++) {
+            let target = 0 - nums[i];
+            let left = i + 1,
+                right = nums.length - 1;
+            while (left < right) {
+                let sum = nums[left] + nums[right];
+                if (sum < target) left++;
+                else if (sum > target) right--;
+                else {
+                    let key = `${nums[i]}${nums[left]}${nums[right]}`;
+                    if (!tripletsMap.has(key))
+                        tripletsMap.set(key, [nums[i], nums[left], nums[right]]);
+                    left++;
+                    right--;
+                }
+            }
+        }
+        tripletsMap.forEach((value) => {
+            result.push(value)
+        })
+        return result
+
+    }
+}
