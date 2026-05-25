c36be3562a3e0dda3bdd8076de82fe8d54200989
Cyuen0310 <chanszeyuen0310@icloud.com>
2026-05-25 13:09:43 +0800

    Add: trapping-rain-water - submission-6

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-6.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number[]} height
+     * @return {number}
+     * trapped formula = min(left max/right max) - current height
+     */
+    trap(height) {
+        let trapped = 0,
+            left = 0,
+            right = height.length - 1,
+            leftMax = height[left],
+            rightMax = height[right];
+
+        while (left < right) {
+            if (leftMax <= rightMax) {
+                trapped += Math.max(leftMax - height[left], 0);
+                left++;
+                leftMax = leftMax < height[left] ? height[left] : leftMax;
+            } else {
+                trapped += Math.max(rightMax - height[right], 0);
+                right--;
+                rightMax = rightMax < height[right] ? height[right] : rightMax;
+            }
+        }
+
+        return trapped;
+    }
+}
