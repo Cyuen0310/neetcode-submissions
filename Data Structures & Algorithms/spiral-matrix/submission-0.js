1a640269344eee03771f5f8ed086bb486d230c31
Cyuen0310 <chanszeyuen0310@icloud.com>
2026-06-15 06:44:17 +0800

    Add: spiral-matrix - submission-0

---
+++ Data Structures & Algorithms/spiral-matrix/submission-0.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @return {number[]}
+     * top -> right -> bottom -> left
+     */
+    spiralOrder(matrix) {
+        let top = 0, left = 0, right = matrix[0].length - 1, bottom = matrix.length - 1
+        let result = []
+        while (top <= bottom && left <= right){
+            for (let i = left; i <= right; i++){
+                result.push(matrix[top][i])
+            }
+            top++
+            if (top > bottom) break
+            for (let i = top; i <= bottom; i++){
+                result.push(matrix[i][right])
+            }
+            right--
+            for (let i = right; i >= left; i--){
+                result.push(matrix[bottom][i])
+            }
+            bottom--
+            if (left > right) break
+            for (let i = bottom; i >= top; i--){
+                result.push(matrix[i][left])
+            }
+            left++
+        }
+        return result
+    }
+}
