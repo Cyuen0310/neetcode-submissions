e245b2d6db3dfc18ae09d85bc3c78e12a8df368c
Cyuen0310 <chanszeyuen0310@icloud.com>
2026-06-17 18:00:29 +0800

    Add: max-consecutive-ones - submission-2

---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-2.js
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    findMaxConsecutiveOnes(nums) {
+        let cur = 0, max = 0
+        for (let num of nums){
+            cur = num ? cur + 1:0
+            max = Math.max(cur, max)
+        }
+        return max
+    }
+}
