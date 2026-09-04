e95c9f6b23c60ee16eb04f4eb4365b68c766d796
Cyuen0310 <chanszeyuen0310@icloud.com>
2026-09-04 17:08:01 +0800

    Add: two-integer-sum - submission-12

---
+++ Data Structures & Algorithms/two-integer-sum/submission-12.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        let seen = new Map()
+
+        for (let i = 0; i < nums.length;i++){
+            let diff = target - nums[i]
+            if (!seen.has(diff)) seen.set(nums[i],i)
+            else return [seen.get(diff), i]
+        }
+    }
+}
